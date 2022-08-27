434a0d215b298063de328ba4b545ace77eae76ae
Char Chen <charliecaresnot@gmail.com>
2022-08-27 03:14:14 +0200

    refactor(tag): scale the alpha factor non-linearly to avoid tags showing too early
    and thus overshadowing another tag easily on the edge of triggers

--- Assets/Scripts/GenderTag.cs
+++ Assets/Scripts/GenderTag.cs
@@ -27,21 +27,24 @@ namespace WordGenderApp
 
         private void Update()
         {
+            float temp_a = 0f;
             switch (_swipeDirection)
             {
                 case Datatypes.SwipeArea.Left:
                 case Datatypes.SwipeArea.Right:
+
                     float x_start = _defaultWordCardPos.x;
                     float x_end = _referenceLine.position.x;
-                    _cg.alpha = Mathf.InverseLerp(x_start, x_end, _wordCard.transform.position.x);
+                    temp_a = Mathf.InverseLerp(x_start, x_end, _wordCard.transform.position.x);
                     break;
                 case Datatypes.SwipeArea.Top:
                 case Datatypes.SwipeArea.Bottom:
                     float y_start = _defaultWordCardPos.y;
                     float y_end = _referenceLine.position.y;
-                    _cg.alpha = Mathf.InverseLerp(y_start, y_end, _wordCard.transform.position.y);
+                    temp_a = Mathf.InverseLerp(y_start, y_end, _wordCard.transform.position.y);
                     break;
             }
+            _cg.alpha = Mathf.Clamp(2 * temp_a - 1f, 0f, 1f);
         }
     }
 }
