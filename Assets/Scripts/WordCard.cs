a72518313c2bd41f3b1920b52c5a40601f554d5f
Char Chen <charliecaresnot@gmail.com>
2022-08-29 00:46:09 +0200

    feat(card): also rotate card when swiping

--- Assets/Scripts/WordCard.cs
+++ Assets/Scripts/WordCard.cs
@@ -14,7 +14,10 @@ namespace WordGenderApp
         private float _swipeAwaySpeed = 120f;
         [SerializeField]
         [Range(0.01f, 0.2f)]
-        private float _returnToCenterDuration = 0.03f;
+        private float _returnToCenterDuration = 0.05f;
+        [SerializeField]
+        [Range(0f, 0.2f)]
+        private float _rotateRate = 0.03f;
 
         [Header("Object References")]
         [SerializeField]
@@ -23,6 +26,7 @@ namespace WordGenderApp
         private WordCardManager _manager;
         private Vector3 _defaultPosition;
         private Vector2 _delta;
+        private bool _fingerDownOnUpperPart;
 
         public Datatypes.WordData WordData { get; private set; }
         public string Word => WordData.Word;
@@ -41,12 +45,17 @@ namespace WordGenderApp
         public void OnDrag(PointerEventData eventData)
         {
             _delta = eventData.delta;
+
             transform.Translate(_delta);
+            float horizDiviation = transform.position.x - _defaultPosition.x;
+            float newAngle = horizDiviation * _rotateRate * (_fingerDownOnUpperPart ? 1f : -1f);
+            transform.localEulerAngles = new Vector3(0f, 0f, newAngle);
         }
 
         public void OnBeginDrag(PointerEventData eventData)
         {
-            
+            var fingerPos = eventData.position;
+            _fingerDownOnUpperPart = fingerPos.y < _defaultPosition.y;
         }
 
         public void OnEndDrag(PointerEventData eventData)
@@ -100,14 +109,20 @@ namespace WordGenderApp
             Vector3 startPos = transform.position;
             Vector3 endPos = _defaultPosition;
 
+            Quaternion startRot = transform.localRotation;
+            Quaternion endRot = Quaternion.identity;
+
             float timer = 0f;
             while (timer < animDuration)
             {
-                transform.position = Vector3.Lerp(startPos, endPos, timer / animDuration);
+                float t = timer / animDuration;
+                transform.position = Vector3.Lerp(startPos, endPos, t);
+                transform.localRotation = Quaternion.Lerp(startRot, endRot, t);
                 yield return null;
                 timer += Time.deltaTime;
             }
             transform.position = _defaultPosition;
+            transform.localRotation = Quaternion.identity;
         }
     }
 }
